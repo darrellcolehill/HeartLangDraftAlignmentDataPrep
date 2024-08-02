e86cfc45161e30a3cf0a61627e21846238bb2d36
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-02 11:46:31 -0400

    initial commit

---
+++ tutorial.js
@@ -0,0 +1,164 @@
+const path = require('path');
+const fse = require('fs-extra');
+const { Proskomma } = require('proskomma');
+const pk = new Proskomma();
+let content = fse.readFileSync(path.resolve(__dirname, './43-LUK-en-UST.usfm')).toString();
+const fs = require('fs');
+
+
+const setup = async function (pk) {
+
+    const mutation = `mutation { addDocument(` +
+    `selectors: [{key: "lang", value: "eng"}, {key: "abbr", value: "ust"}], ` + // TODO: set this dynamically
+    `contentType: "usfm", ` +
+    `content: """${content}""") }`;
+
+    const result = await pk.gqlQuery(mutation);
+    let cvData = result.data
+    console.log(JSON.stringify(result, null, 2));
+ }
+
+
+// DATA TYPES THAT I NEED TO PARSE INTO
+//  type GreekAlignmentData = {
+// 	strong: string;
+// 	morph?: string;
+// }
+
+// type AlignedText = {
+// 	text: string;
+// 	greekAlignmentData?: GreekAlignmentData[]
+// }
+
+
+// type AlignedVerse = {
+// 	verseNum: number;
+// 	alignedVerseText: AlignedText[];
+// }
+
+
+function getStrongs(str) {
+    const regex = /\b[Gg]\d+\b/;
+    const match = str.match(regex);
+    
+    if (match) {
+      const extracted = match[0];
+    //   console.log(extracted);
+      return extracted
+    } else {
+      console.log("No match found.");
+      // TODO: return undefined
+    }
+}
+
+function getLemma(str) {
+    const regex = /[^/]+$/;
+    const lemma = str.match(regex)[0];
+    return lemma
+}
+
+
+ const queryPk = async function (pk, query) {
+    const result = await pk.gqlQuery(query);
+    let cvData = result.data.documents[0].cv[0].items.filter((item) => 
+        item.payload === "milestone/zaln" || item.subType === "wordLike" 
+        || (item.payload.includes("x-strong") && item.subType === "start")
+        || (item.payload.includes("x-lemma") && item.subType === "start")
+        || item.payload.includes("verse/")
+    )
+
+    let alignedVerses = [];
+    let alignedText = [];
+    let alignStartCount = 0; 
+    let text = "";
+    let greekAlignmentData = []
+    let verseNum = 1;
+
+    for(let i = 0; i < cvData.length; i++) {
+        let attribute = cvData[i];
+
+        if(attribute.subType === "start" && attribute.payload === "milestone/zaln") {
+            alignStartCount++; 
+        } else if(attribute.subType === "end" && attribute.payload === "milestone/zaln") {
+            alignStartCount--; 
+        } else if(attribute.subType === "wordLike") {
+            let startingChar = text ===   "" ? "" : " "
+            text += startingChar + attribute.payload;
+        } else if(attribute.subType === "start" && attribute.payload.includes("x-strong")) {
+            greekAlignmentData.push({strong: getStrongs(attribute.payload)});
+        } else if(attribute.subType === "start" && attribute.payload.includes("x-lemma")) {
+            greekAlignmentData.push({lemma: getLemma(attribute.payload)});
+        } else if(attribute.subType === "end" && attribute.payload.includes("verse/")) {
+            alignedVerses.push({verseNum: verseNum, alignedVerseText: alignedText})
+            verseNum++;
+            alignedText = []
+        }
+
+        if(alignStartCount === 0 && text != "") {
+
+            // Push alignment text
+            if(greekAlignmentData.length > 0) {
+                alignedText.push({text: text, greekAlignmentData: greekAlignmentData});
+            } else {
+                alignedText.push({text: text});
+            }
+
+            // reset buffers
+            text = "";
+            greekAlignmentData = [];
+        }
+    }
+
+    // Convert the object to a JSON string
+    const jsonString = JSON.stringify(alignedVerses, null, 2);
+
+    // Define the file path
+    const filePath = './sampleObject.json';
+
+    // Write the JSON string to a file
+    fs.writeFile(filePath, jsonString, (err) => {
+        if (err) {
+            console.error('Error writing to file', err);
+        } else {
+            console.log('JSON file has been saved');
+        }
+    });
+ }
+
+
+
+// TODO NOTE: this one is key. Notice that the start and end zaln's are noticed and they enclose some amount
+//  of wordLike tags. It also contains the strongs information as an attribute. 
+// const dataQuery = `
+// {
+//     documents {
+//         id
+//         cv(chapter: "1", verses: ["1"]) {
+//             items {
+//                 subType
+//                 payload
+//             }
+//         }
+//     }
+// }
+// `;
+
+// TODO NOTE: this gets all the verses for the chapter. 
+const dataQuery = `
+{
+    documents {
+        id
+        cv(chapter: "1") {
+            items {
+                subType
+                payload
+            }
+        }
+    }
+}
+`;
+
+
+setup(pk);
+
+queryPk(pk, dataQuery);
