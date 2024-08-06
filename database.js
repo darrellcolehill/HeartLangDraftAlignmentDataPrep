10dee65da1489298504401865200e2e02a1a6a59
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-06 16:47:07 -0400

    Updated database code

--- database.js
+++ database.js
@@ -1,16 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('mydatabase.db');
+let db = new sqlite3.Database('alignedData.db');
 
 function initialize() {
     db.run(`CREATE TABLE IF NOT EXISTS verses (
         id INTEGER PRIMARY KEY,
-        languageCode: TEXT NOT NULL,
-        version: TEXT NOT NULL,
+        languageCode TEXT NOT NULL,
+        version TEXT NOT NULL,
         book TEXT NOT NULL,
         chapter TEXT NOT NULL,
-        verse TEXT NOT NULL,
-        interleavedVerse TEXT NOT NULL
+        verse INTEGER NOT NULL,
+        verseText TEXT NOT NULL,
+        interleavedVerseText TEXT NOT NULL
     )`, (err) => {
         if (err) {
             return console.error(err.message);
@@ -19,30 +20,31 @@ function initialize() {
     });
 }
 
-function insert(languageCode, version, book, chapter, verse, interleavedVerse) {
-    if (!languageCode || !version || !book || !chapter || !verse || !interleavedVerse) {
-        console.error(`Invalid insert values: '${languageCode}', '${version}', '${book}', '${chapter}', '${verse}', '${interleavedVerse}'`);
+async function insert(languageCode, version, book, chapter, verse, verseText, interleavedVerseText) {
+    if (!languageCode || !version || !book || !chapter || !verse || !verseText || !interleavedVerseText) {
+        console.error(`Invalid insert values: '${languageCode}', '${version}', '${book}', ${chapter}, ${verse}, '${verseText}', '${interleavedVerseText}'`);
         return;
     }
 
-    db.run(`INSERT INTO verses (languageCode, version, book, chapter, verse, interleavedVerse) 
-            VALUES (?, ?, ?, ?)`, [languageCode, version, book, chapter, verse, interleavedVerse], (err) => {
+    db.run(`INSERT INTO verses (languageCode, version, book, chapter, verse, verseText, interleavedVerseText) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`, [languageCode, version, book, chapter, verse, verseText, interleavedVerseText], (err) => {
         if (err) {
+            console.log(`'${languageCode}', '${version}', '${book}', '${chapter}', '${verse}', '${verseText}', '${interleavedVerseText}'`)
             return console.error(err.message);
         }
         console.log('Row inserted.');
     });
 }
 
-function update(id, languageCode, version, book, chapter, verse, interleavedVerse) {
-    if (!id || !languageCode || !version || !book || !chapter || !verse || !interleavedVerse) {
-        console.error(`Invalid update values: '${id}', '${languageCode}', '${version}', '${book}', '${chapter}', '${verse}', '${interleavedVerse}'`);
+function update(id, languageCode, version, book, chapter, verse, verseText, interleavedVerseText) {
+    if (!id || !languageCode || !version || !book || !chapter || !verse || !verseText || !interleavedVerseText) {
+        console.error(`Invalid update values: '${id}', '${languageCode}', '${version}', '${book}', ${chapter}, ${verse}, '${verseText}', '${interleavedVerseText}'`);
         return;
     }
 
     db.run(`UPDATE verses 
-            SET languageCode = ?, version = ?, book = ?, chapter = ?, verse = ?, interleavedVerse = ? 
-            WHERE id = ?`, [languageCode, version, book, chapter, verse, interleavedVerse, id], (err) => {
+            SET languageCode = ?, version = ?, book = ?, chapter = ?, verse =?, verseText = ?, interleavedVerseText = ? 
+            WHERE id = ?`, [languageCode, version, book, chapter, verse, verseText, interleavedVerseText, id], (err) => {
         if (err) {
             return console.error(err.message);
         }
