a13f710475108d9cf95d79d868811c57bccb38b5
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-06 16:09:25 -0400

    Partial populate database script
    Still need to actually add the stuff to the database

---
+++ populateDatabase.js
@@ -0,0 +1,140 @@
+const path = require('path');
+const fse = require('fs-extra');
+const fs = require('fs');
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+const INPUT_DIRECTORY_ROOT = "./output"
+
+// TODO: update this so that it works with other language codes like "hi"
+// TODO: add this to global constants file
+const languageCodeMap = {
+    "en": "eng",
+    "hi": "hin"
+}
+
+// DONE: get all sub-folders in the output folder
+
+    // DONE: Extract lang code and version from folder name
+
+    // DONE: For each book
+
+        // DONE: For each chapter
+
+            // DONE: For each verse
+
+                // DONE: map verse into something that has "verse" and "interleavedVerse"
+
+                // TODO: insert into DB
+
+async function populate() {
+    let directories = await getDirectories(INPUT_DIRECTORY_ROOT)
+
+    for(let i = 0; i < directories.length; i++) {
+        let {language, version} = getLanguageAndVersion(directories[i])
+        let jsonFiles = await getJsonFiles(`${INPUT_DIRECTORY_ROOT}/${directories[i]}`)
+
+        for(let j = 0; j < jsonFiles.length; j++) {
+            const jsonFileContentPath = `${INPUT_DIRECTORY_ROOT}/${directories[i]}/${jsonFiles[j]}`
+            console.log(`Adding: ${jsonFileContentPath}`)
+
+                try {
+                    const data = fs.readFileSync(jsonFileContentPath, 'utf8');
+                    const jsonData = JSON.parse(data);
+                    let {bookName, chapter} = getBookNameAndChapter(jsonFiles[j])
+                    addChapterToDatabase(language, version, bookName, chapter, jsonData)
+                } catch (err) {
+                    console.error('Error reading the JSON file:', err);
+                }  
+
+        }
+    }
+}
+
+
+async function getDirectories(source) {
+    return (await readdir(source, { withFileTypes: true }))
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+}
+
+
+function getLanguageAndVersion(projectName) {
+
+    // Extracts the language and version from aligned bibles on Door43 from projects in the from of
+    // langCode_version
+    const repoNamePattern = /^([^_]+)_(.+)$/;
+    const match = projectName.match(repoNamePattern);
+
+    if (match) {
+        const language = match[1]; 
+        const version = match[2];
+        return {language: languageCodeMap[language], version: version}
+    } else {
+        console.error(`Could not extract language code and version from project ${projectName}.`);
+    }
+}
+
+
+async function getJsonFiles(directory) {
+    try {
+        const files = await readdir(directory);
+        return files.filter(file => path.extname(file).toLowerCase() === '.json');
+    } catch (err) {
+        console.error('Error reading directory:', err);
+        return [];
+    }
+}
+
+
+function getBookNameAndChapter(str) {
+    const regex = /^([A-Z]+)-(\d+)\.json$/;
+    const match = str.match(regex);
+    
+    if (match) {
+        const book = match[1];
+        const chapter = match[2];
+        return {bookName: book, chapter: chapter}
+    } else {
+        console.error('No match found.');
+    }
+}
+
+
+function addChapterToDatabase(languageCode, version, bookName, chapterNumber, chapterContent) {
+
+    chapterContent.forEach(alignedVerse => {
+        let plainText = ""
+        let interleavedText = ""
+
+        alignedVerse.alignedVerseText.forEach(alignedWord => {
+
+            // TODO: ask drew if this is correct. There are a lot of cases with just o <LEMMA>
+            const lemmas = alignedWord?.greekAlignmentData.map(it => {
+                return it.lemma
+            }).join(" ")
+
+            let leadingSpace = ""
+            if(plainText !== "" || interleavedText !== "") {
+                leadingSpace = " "
+            }
+
+            // Add word
+            plainText = `${plainText}${leadingSpace}${alignedWord.text}`
+
+            // If possible, add work with lemma, otherwise, just add the word
+            if(lemmas && lemmas.length > 0) {
+                interleavedText = `${interleavedText}${leadingSpace}${alignedWord.text} ${lemmas}`
+            } else {
+                interleavedText = `${interleavedText}${leadingSpace}${alignedWord.text}`
+            }
+        })
+
+        const x = 1
+
+    });
+
+}
+
+
+populate()
