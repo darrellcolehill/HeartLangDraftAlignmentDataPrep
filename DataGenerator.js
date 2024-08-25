a0dedc0b50037cda411e20129741faa7108a70df
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-25 12:05:23 -0400

    Includes punctuation in output

--- DataGenerator.js
+++ DataGenerator.js
@@ -17,7 +17,7 @@ const { getLanguageAndVersion, getDirectories } = require('./utils/index')
 class DataGenerator {
         
     // Allows for async data generation
-    async setupLangVersion(pk, language, version) {
+    async addBibleToGraph(pk, language, version) {
 
         let usfmFiles = await this.getUsfmFiles(`${INPUT_DOCUMENTS_PATH}/${language}_${version}`);
 
@@ -33,7 +33,7 @@ class DataGenerator {
         
     }
 
-    async setup(pk) {
+    async addAllBiblesToGraph(pk) {
         console.log("===== ADDING DOCUMENTS TO GRAPH =====")
         let directories = await getDirectories(`${INPUT_DOCUMENTS_PATH}`)
 
@@ -118,18 +118,32 @@ class DataGenerator {
 
         const result = await pk.gqlQuery(dataQuery);
         let cvData = result.data.docSet.documents[0].cv[0].items.filter((item) => 
-            item.payload === "milestone/zaln" || item.subType === "wordLike" 
-            || (item.payload.includes("x-strong") && item.subType === "start")
-            || (item.payload.includes("x-lemma") && item.subType === "start")
-            || item.payload.includes("verse/")
+            item.payload === "milestone/zaln" 
+                || item.subType === "wordLike" 
+                || (item.payload.includes("x-strong") && item.subType === "start")
+                || (item.payload.includes("x-lemma") && item.subType === "start")
+                || (item.payload.includes("x-content") && item.subType === "start")
+                || (item.payload.includes("verse/"))
+                || (item.subType === "punctuation")
         )
 
         let alignedVerses = [];
         let alignedText = [];
+        // This acts like a stack. Whenever a "start" tag is found we increment, and whenever an "end" tag
+        //  is found, we decrement. Once we are at 0, that means that we have consumed content for an 
+        //  entire word, and we can add that alignment data to the appropriate accumulator. 
         let alignStartCount = 0; 
         let text = "";
         let greekAlignmentData = []
+        let punctuationData = []
         let verseNum = 1;
+
+        // Adds items to the greekAlignmentData accumulator using the following rules. This utilizes the order in which data appears. 
+        //  strongs, then lemma, then content. 
+        // 1) Create a new greekAlignmentData object when a strongs is encountered and add it to the greekAlignmentData
+        //  accumulator
+        // 2) Add lemma to last element in greekAlignmentData
+        // 3) Add content to last element in greekAlignmentData
 
         for(let i = 0; i < cvData.length; i++) {
             let attribute = cvData[i];
@@ -141,16 +155,27 @@ class DataGenerator {
             } else if(attribute.subType === "wordLike") {
                 let startingChar = text ===   "" ? "" : " "
                 text += startingChar + attribute.payload;
-            } else if(attribute.subType === "start" && attribute.payload.includes("x-strong")) {
-                const strong = this.getStrongs(attribute.payload)
-
-                const gwtContent = await this.getGreekWordContent(strong)
-                if(gwtContent) {
-                    const lemma = await this.getLemmaFromGWTContent(gwtContent)
-                    if(lemma) {
-                        greekAlignmentData.push({strong: strong, lemma: lemma});
-                    }
+            } 
+            else if(attribute.subType === "punctuation") {
+                // If text is empty, append this to the last text, otherwise, append it to the current string
+                if(text === "" && alignedText?.length > 0) {
+                    const lastWordPunctuationData = alignedText[alignedText.length - 1].punctuationData
+                    lastWordPunctuationData.push(attribute.payload)
+                } else {
+                    punctuationData.push(attribute.payload)
                 }
+            } 
+            else if(attribute.subType === "start" && attribute.payload.includes("x-strong")) {
+                const strong = this.getFiveDigitStrongs(attribute.payload)
+                greekAlignmentData.push({strong: strong});
+
+            } else if(attribute.subType === "start" && attribute.payload.includes("x-lemma")) {
+                let lemma = this.getLemmaFromUsfm(attribute.payload)
+                greekAlignmentData[greekAlignmentData.length - 1]["lemma"] = lemma
+
+            } else if(attribute.subType === "start" && attribute.payload.includes("x-content")) {
+                let content = this.getContentFromUSFM(attribute.payload)
+                greekAlignmentData[greekAlignmentData.length - 1]["content"] = content
 
             } else if(attribute.subType === "end" && attribute.payload.includes("verse/")) {
                 alignedVerses.push({verseNum: verseNum, alignedVerseText: alignedText})
@@ -158,18 +183,21 @@ class DataGenerator {
                 alignedText = []
             }
 
+            // TODO: make sure that this is handling the last word in each verse correctly. 
+            //  I am concerned with the "else if" abover that it is not
             if(alignStartCount === 0 && text != "") {
 
                 // Push alignment text
                 if(greekAlignmentData.length > 0) {
-                    alignedText.push({text: text, greekAlignmentData: greekAlignmentData});
+                    alignedText.push({text: text, greekAlignmentData: greekAlignmentData, punctuationData: punctuationData});
                 } else {
-                    alignedText.push({text: text});
+                    alignedText.push({text: text, punctuationData: punctuationData});
                 }
 
                 // reset buffers
                 text = "";
                 greekAlignmentData = [];
+                punctuationData = []
             }
         }
 
@@ -178,8 +206,22 @@ class DataGenerator {
 
 
     // Gets 4 digit strongs. In the case of G12345, it matches on G1234. 
-    getStrongs(str) {
+    getFourDigitStrongs(str) {
         const regex = /\bG(\d{4})\d*\b/;
+        const match = str.match(regex);
+        
+        if (match) {
+            const strong = 'g' + match[1];
+            return strong;
+        } else {
+            console.error(`Could not match strongs for: ${str}`);
+        }
+    }
+
+
+    // Gets 5 digit strongs. In the case of G12345, it matches on G1234. 
+    getFiveDigitStrongs(str) {
+        const regex = /\bG(\d{5})\d*\b/;
         const match = str.match(regex);
         
         if (match) {
@@ -282,6 +324,21 @@ class DataGenerator {
     }
 
 
+    // NOTE: this is really the same as getLemmaFromUSFM, but I am keeping them separate for now until things are more finalized
+    // TODO: see if I can combine this and getLemmaFromUSFM when the dust has set.
+    getContentFromUSFM(str) {
+        const regex = /[^/]+$/;
+        const match = str.match(regex);
+
+        if (match[0]) {
+            const lemma = match[0];
+            return lemma
+        } else {
+            console.error("No match found.");
+        }
+    }
+
+
     writeAlignedVersesToJson(docSetID, bookCode, chapter, alignedVerses) {
 
         const jsonString = JSON.stringify(alignedVerses, null, 2);
@@ -363,13 +420,12 @@ class DataGenerator {
         const pk = new Proskomma();
 
         if(!language && !version) {
-            await this.setup(pk)
+            await this.addAllBiblesToGraph(pk)
         } else {
-            await this.setupLangVersion(pk, language, version)
+            await this.addBibleToGraph(pk, language, version)
         }
         this.processDocuments(pk)
     }
 }
-
 
 module.exports = DataGenerator;
